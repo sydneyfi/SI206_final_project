da01c4709a4097464f6d11b334901929f4c7e2f0
selenaw <selenaw@umich.edu>
2023-04-11 10:33:56 -0400

    still in progress

--- stockInfo.py
+++ stockInfo.py
@@ -3,14 +3,18 @@ import sqlite3
 import certifi
 import json
 import os
+import requests
 
 
 # financial modeling prep API Key: 7b82b2f514ddca127fb725b5c725eb67
 
 def get_jsonparsed_data(url):
-    response = urlopen(url, cafile=certifi.where())
-    data = response.read().decode("utf-8")
-    return json.loads(data)
+    # response = urlopen(url, cafile=certifi.where())
+    # data = response.read().decode("utf-8")
+    # return json.loads(data)
+
+    data = requests.get(url)
+    return data.json()
 
 
 def make_db(db):
@@ -26,16 +30,31 @@ def main():
     url2 = '?apikey=7b82b2f514ddca127fb725b5c725eb67'
 
     cur, conn = make_db('Companies.db')
+
     cur.execute(
         '''
         CREATE TABLE IF NOT EXISTS Financial
-        (ranking INTEGER PRIMARY KEY, stock_price FLOAT, website STRING)
+        (id INTEGER PRIMARY KEY, stock_price FLOAT, website TEXT)
         '''
     )
 
-    cur.execute('SELECT * FROM ')
+    cur.execute('SELECT id, ticker FROM Website')
+    webs = cur.fetchall()
 
-    data_dict = get_jsonparsed_data(url1 + ticker + url2)[0]
+    for w in webs:
+        ticker = w[1]
+        data_dict = get_jsonparsed_data(url1 + ticker + url2)[0]
+
+        price = data_dict['price']
+        website = data_dict['website']
+
+        # print((w[0], price, website))
+
+        cur.execute('INSERT OR IGNORE INTO Financial (id, stock_price, website) VALUES (?,?,?)', (int(w[0]), float(price), website))
+
+    conn.commit()
+
+    # if cur.fetchone() == None:
 
     
 
